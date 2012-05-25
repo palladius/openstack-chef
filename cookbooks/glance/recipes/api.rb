a9a8939403342bf01a2a46dbdfd3a74be87f5230
Dan Prince <dprince@redhat.com>
2012-05-25 14:35:07 -0400

    Set sql_connection variable in glance::api recipe.
    Change-Id: I2d738a910ab7caf21c1589e3de663b48a4dec780

--- cookbooks/glance/recipes/api.rb
+++ cookbooks/glance/recipes/api.rb
@@ -6,6 +6,43 @@
 
 include_recipe "#{@cookbook_name}::common"
 
+sql_connection = nil
+if node[:glance][:mysql]
+  Chef::Log.info("Using mysql")
+  package "python-mysqldb"
+  mysqls = nil
+
+  unless Chef::Config[:solo]
+    mysqls = search(:node, "recipes:glance\\:\\:mysql")
+  end
+  if mysqls and mysqls[0]
+    mysql = mysqls[0]
+    Chef::Log.info("Mysql server found at #{mysql[:mysql][:bind_address]}")
+  else
+    mysql = node
+    Chef::Log.info("Using local mysql at  #{mysql[:mysql][:bind_address]}")
+  end
+  sql_connection = "mysql://#{mysql[:glance][:db][:user]}:#{mysql[:glance][:db][:password]}@#{mysql[:mysql][:bind_address]}/#{mysql[:glance][:db][:database]}"
+elsif node[:glance][:postgresql]
+  Chef::Log.info("Using postgresql")
+  postgresqls = nil
+
+  unless Chef::Config[:solo]
+    postgresqls = search(:node, "recipes:glance\\:\\:postgresql")
+  end
+  if postgresqls and postgresqls[0]
+    postgresql = postgresqls[0]
+    Chef::Log.info("PostgreSQL server found at #{postgresql[:ipaddress]}")
+  else
+    postgresql = node
+    Chef::Log.info("Using local PostgreSQL at #{postgresql[:ipaddress]}")
+  end
+  sql_connection = "postgresql://#{postgresql[:glance][:db][:user]}:#{postgresql[:glance][:db][:password]}@#{postgresql[:ipaddress]}/#{postgresql[:glance][:db][:database]}"
+else
+  # default to sqlite
+  sql_connection = "sqlite:////var/lib/glance/glance.sqlite"
+end
+
 # Locate glance registry and retrieve it's IP
 unless Chef::Config[:solo]
   registries = search(:node, "recipes:glance\\:\\:registry")
@@ -61,6 +98,9 @@ template node[:glance][:api_config_file] do
   owner "glance"
   group "glance"
   mode 0644
+  variables(
+    :sql_connection => sql_connection
+  )
 end
 
 template node[:glance][:cache_config_file] do
